b40085e5d018c0ea308656ea9fabd9118fded83f
thippesw <thippesw@xilinx.com>
2021-09-23 11:25:06 -0600

    notebooks and xmediactl, xrt removed

---
+++ petalinux/xilinx-vmk180-trd/project-spec/meta-vmk180-trd/recipes-apps/xmediactl/files/xmediactl.sh
@@ -0,0 +1,13 @@
+echo "setting IMX274 to SRGBB 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"IMX274 2-001a":0 [fmt:SRGGB8_1X8/3840x2160@1/30 field:none]'
+echo "setting csiss to SRGBB 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"a4060000.csiss":0 [fmt:SRGGB8_1X8/3840x2160 field:none]'
+echo "setting demosaic:0  SRGBB 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"a40c0000.v_demosaic":0 [fmt:SRGGB8_1X8/3840x2160 field:none]'
+echo "setting demosaic:1  RGB24 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"a40c0000.v_demosaic":1 [fmt:RBG24/3840x2160 field:none]'
+echo "setting scalar:0  RGB24 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"a4080000.scaler":0 [fmt:RBG24/3840x2160 field:none]'
+echo "setting scalar:1  UYVy8_1X16 3840x2160"
+xmedia-ctl -d /dev/media1 -V '"a4080000.scaler":1 [fmt:UYVY8_1X16/3840x2160 field:none]'
+
